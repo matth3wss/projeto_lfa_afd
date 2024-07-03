d903470c0189a5fa53ee94d3f02f96ffb60a5610
Matheus Henrique Rodrigues da Costa <matheus.r@compasso.com.br>
2024-07-03 16:30:15 -0300

    :hammer: refac: Refactor RegexPatterns.py to use consistent quotation marks in regular expressions

--- classes/RegexPatterns.py
+++ classes/RegexPatterns.py
@@ -4,15 +4,15 @@ import re
 class RegexPatterns:
     @classmethod
     def symbol(cls, text):
-        pattern = r'<[A-Z]> ::='
+        pattern = r"<[A-Z]> ::="
         return re.findall(pattern, text)
 
     @classmethod
     def symbol_alt(cls, text):
-        pattern = r'<[A-Z]>'
+        pattern = r"<[A-Z]>"
         return re.findall(pattern, text)
 
     @classmethod
     def variable(cls, text):
-        pattern = r'([a-z])<([A-Z])>'
+        pattern = r"([a-z])<([A-Z])>"
         return re.findall(pattern, text)
